16316d7e29905119129dab20f869c75361777f34
yanmeiyu <784519047@qq.com>
2020-04-16 17:10:35 +0800

    提交消息跳转

--- app/pages/message/mesgsDetail.js
+++ app/pages/message/mesgsDetail.js
@@ -6,92 +6,103 @@
  * @Description: In User Settings Edit
  * @FilePath: \ltx\app\pages\message\mesgsDetail.js
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import "./style/index.less";
 import Icon from 'antd/es/icon';
 import 'antd/es/icon/style';
 import {withRouter} from "react-router-dom";
 import axios from 'axios';
 import commonUrl from '../../config';
-import { WhiteSpace } from 'antd-mobile';
+import {WhiteSpace, Toast} from 'antd-mobile';
 import {connect} from 'react-redux';
 import noAuth from '../../util/noAuth';
+const test = "http://127.0.0.1:8088"
 class mesgsDetail extends Component {
-    state={
-        mesgsList:[]
+    state = {
+        mesgsList: []
     }
-    componentWillMount(){
-        const {history,location}=this.props;
-        const {params:{title,id,params}}=location;
-        axios.post(`${commonUrl}/app/qryMsgRecordList.do`,{userId:9984,recordId:id,params})
-        .then(res=>{
-            noAuth(res.data,()=>this.props.history.push('/login'))
-            if(res.data.code==='success'){
-                this.setState({mesgsList:res.data.data})
-            }
-        })
+
+    componentWillMount() {
+        const {history, location} = this.props;
+        const {params: {title, id, params}} = location;
+        axios.post(`${commonUrl}/app/qryMsgRecordList.do`, {userId: 9984, recordId: id, params})
+            .then(res => {
+                noAuth(res.data, () => this.props.history.push('/login'));
+                if (res.data.code === 'success') {
+                    this.setState({mesgsList: res.data.data})
+                }
+            })
     }
-    readinfo=({id,params})=>{
-        axios.post(`${commonUrl}/app/readMsgRecord.do`,{userId:9984,recordId:id,params})
-        .then(res=>{
-            noAuth(res.data,()=>this.props.history.push('/login'))
-            if(res.data.code==='success'){
-            }
-        })
-    }
+
+    readinfo = ({id, params, type}) => {
+        if (type === 4) {
+            const {history, location} = this.props;
+            let fomatParams = JSON.parse(params);
+            history.replace({pathname:`/zbdetail/${fomatParams.id}`})
+        } else {
+            Toast.info("请到电脑上查看消息详情！");
+        }
+        axios.post(`${commonUrl}/app/readMsgRecord.do`, {userId: 9984, recordId: id, params})
+            .then(res => {
+                noAuth(res.data, () => this.props.history.push('/login'));
+                if (res.data.code === 'success') {
+                }
+            })
+    };
+
     render() {
-        const {history,location}=this.props;
-        const {params:{title,id,params}}=location;
+        const {history, location} = this.props;
+        const {params: {title, id, params}} = location;
         return (
             <div className="mesgDetail">
                 <div className="mesgDetail_topbar">
                     <span>{title}</span>
                     <Icon
-                    onClick={() => this.props.history.goBack()}
-                    style={{
-                        position: "absolute",
-                        left: ".1rem",
-                        top: "50%",
-                        color: "#F7F8F4",
-                        fontSize: ".24rem",
-                        transform: "translateY(-50%)"
-                    }} type="left" />
+                        onClick={() => this.props.history.goBack()}
+                        style={{
+                            position: "absolute",
+                            left: ".1rem",
+                            top: "50%",
+                            color: "#F7F8F4",
+                            fontSize: ".24rem",
+                            transform: "translateY(-50%)"
+                        }} type="left"/>
                 </div>
                 {
-                    this.state.mesgsList.map((item,index)=>(
-                        <div key={index} className="mesgDetail_item" onClick={()=>this.readinfo(item)}>
-                        <WhiteSpace size="lg"/>
-                        <div className="date"><span>{item.createTime}</span></div>
-                        <div className="mesgDetail_item_card">
-                            <div className="title">
-                            {
-                                item.readed&&<span className="dot"/>
-                            }
-                            <span className="word">{item.title}</span></div>
-                            <div className="content">{item.content}</div>
-                            <div className="detail">查看详情
-                            <Icon
-                                style={{
-                                    position: "absolute",
-                                    right: ".1rem",
-                                    top: "50%",
-                                    color: "#bbb",
-                                    fontSize: ".24rem",
-                                    transform: "translateY(-50%)"
-                                }} type="right" />
+                    this.state.mesgsList.map((item, index) => (
+                        <div key={index} className="mesgDetail_item" onClick={() => this.readinfo(item)}>
+                            <WhiteSpace size="lg"/>
+                            <div className="date"><span>{item.createTime}</span></div>
+                            <div className="mesgDetail_item_card">
+                                <div className="title">
+                                    {
+                                        item.readed && <span className="dot"/>
+                                    }
+                                    <span className="word">{item.title}</span></div>
+                                <div className="content">{item.content}</div>
+                                <div className="detail">查看详情
+                                    <Icon
+                                        style={{
+                                            position: "absolute",
+                                            right: ".1rem",
+                                            top: "50%",
+                                            color: "#bbb",
+                                            fontSize: ".24rem",
+                                            transform: "translateY(-50%)"
+                                        }} type="right"/>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     ))
                 }
-                    
-                </div>
+
+            </div>
         );
     }
 }
-const mapStateToProps=(state,onwporps)=>{
-    return{
-        userId:state.userinfo.id
+const mapStateToProps = (state, onwporps) => {
+    return {
+        userId: state.userinfo.id
     }
 }
 export default connect(mapStateToProps)(withRouter(mesgsDetail));
