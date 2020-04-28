9af68a5b75b40c143477083441a0e75600cb532d
yanmeiyu <784519047@qq.com>
2020-04-28 09:54:03 +0800

    消息

--- app/components/footerBar/footerbar.js
+++ app/components/footerBar/footerbar.js
@@ -20,10 +20,53 @@ class footerbar extends Component {
         document.addEventListener("plusready", function () {
             // 扩展API加载完毕，现在可以正常调用扩展API
             // 添加监听从系统消息中心点击某条消息启动应用事件
-            plus.push.addEventListener("click", function (msg) {
-                // 分析msg.payload处理业务逻辑
-                console.log("You clicked: " + msg.content);
-            });
+            //监听系统通知栏消息点击事件
+            plus.push.addEventListener("click", function(msg) {
+                // if ( msg.aps ){
+                // 	// 个推pushAPNMessageToSingle接口推送的aps消息
+                // 	var jsonPayload = JSON.parse(msg.aps.category);//通过category实现透传字段传值
+                // 	//实现打开应用的特定业务单据详情页面
+                // 	wrhFunc.openWin('/html/share/shareDetail.html',{bussId:jsonPayload.shareId});
+                // 	return;
+                // }
+                // // 点击应用在前端时，创建的本地消息
+                // var jsonPayload = JSON.parse(msg.payload);
+                // if("YES" == jsonPayload.localMSG){
+                // 	wrhFunc.openWin('/html/share/shareDetail.html',{shareId:jsonPayload.shareId});
+                // }
+                // plus.push.createMessage("...click..." + channelId);
+                console.log("...click..." + JSON.stringify(msg));
+                plus.nativeUI.toast("请到电脑上查看消息详情！");
+            }, false );
+
+            // 获取个推服务端编程透传的消息，
+            plus.push.addEventListener("receive", function(msg) {
+                console.log("deviceType" + window.deviceType + "...receive..." + JSON.stringify(msg));
+                var msgPushVo = JSON.parse(msg.content);
+                var playloadObj = {type: msgPushVo.type, params: msgPushVo.params, way: "LocalMsg"};
+                if (window.deviceType == "Android" || (window.deviceType == "iOS" && msg.payload.way != "LocalMSG")) {
+                    // Android 消息处理 {"__UUID__":"androidPushMsg3649915","title":"福建老干部","content":"后台内容","payload":"后台内容"}
+                    plus.push.createMessage(msgPushVo.msgBody, playloadObj);
+                    return;
+                }
+                // if (window.deviceType == "iOS" && msg.payload.way != "LocalMSG") {
+                // 	// ios 消息处理
+                // 	plus.push.createMessage(msg.content, "LocalMSG");
+                // }
+                if (msg.aps){
+                    // 服务器发来的消息
+                    var jsonPayload = JSON.parse(msg.aps.category);
+                    // 创建本地消息
+                    if('NO' == jsonPayload.localMSG){//个推推送的消息
+                        // 创建本地消息提示,并延迟显示
+                        var messageOption = {cover:false,delay:1};
+                        jsonPayload.localMSG = "YES";
+                        // iOS下，创建本地消息没有声音。
+                        plus.push.createMessage("好友发布了分享，请点击查看", JSON.stringify(jsonPayload), messageOption );
+                    }
+                    plus.push.createMessage("...服务器发送过来..." + channelId, "LocalMSG");
+                }
+            }, false);
         }, false);
     }
 
